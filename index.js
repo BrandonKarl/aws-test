4efac8e2c08f8b326a8b6863e43e1d2efe396629
Brandon Karl <brandon@bkarl.com>
2017-12-11 13:48:22 -0500

    post worked

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ import express from 'express'
 import axios from 'axios'
 const app = express()
 app.get('/', async (req, res) => {
-  const user = {
-    firstName: "test22",
-    lastName: "test3",
-    email: "omg22@gmail.com",
-    password: "password"
-  }
-  const res2 = await axios.post('https://pelycan-backend.herokuapp.com/api/signup', user)
-  res.send('OI')
+  res.send('EZ DEPLOY')
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'))
