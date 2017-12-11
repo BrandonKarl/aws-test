c0002c7828eccb71051bfb434b049f9a9309a9c4
Brandon Karl <brandon@bkarl.com>
2017-12-11 13:43:56 -0500

    testing

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from 'express'
-const axios = require('axios')
+import axios from 'axios'
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY! IT WORKED!!! EZ DEPLOY')
+app.get('/', async (req, res) => {
+  const user = {
+    firstName: "test2",
+    lastName: "test3",
+    email: "omg@gmail.com",
+    password: "password"
+  }
+  const res2 = await axios.post('https://pelycan-backend.herokuapp.com/api/signup', user)
+  res.send('OI')
 })
+
 app.listen(3000, () => console.log('Server running on port 3000'))
